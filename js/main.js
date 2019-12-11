69b1ac0e88fa178e6d80db32a8065a50376dfda4
Lynsphere <58320586+Lynsphere@users.noreply.github.com>
2019-12-11 14:30:56 +0900

    Add files via upload
    인디케이터 동작 수정

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ $(function(){
     var count = 1;  // 이미지번호
 
     // 인터벌함수(함수, 재생시간ms)
-    setInterval(function(){
+    /* setInterval(function(){
         console.log(count);
         if(count > 2) count = 0;
         // 모든 이미지 비표시
@@ -38,5 +38,68 @@ $(function(){
         .addClass('show');
 
         count++;
-    }, 5000);
+    }, 5000); */
+
+    function set_slider(c){
+        $('.slider img, .indicator  a')
+            .removeClass('show');
+
+        count = c;
+
+        $('.slider img')
+            .eq(c)
+            .addClass('show');
+
+        $('.indicator  a')
+            .eq(c)
+            .addClass('show');
+    };
+
+
+    function slider(){
+        setInterval(function(){
+            console.log(count);
+            if(count > 2) count = 0;
+            // 모든 이미지 비표시
+            $('.slider img').removeClass('show');
+            // 현재 이미지만 표시
+            $('.slider img')
+                .eq(count)
+                .addClass('show');
+            
+            // 인디게이터 이동
+            $('.indicator > a').removeClass('show');
+            $('.indicator > a')
+            .eq(count)
+            .addClass('show');
+    
+            count++;
+        }, 5000);
+    };
+
+    set_slider(0);
+    slider();
+
+    $('#in01').click(function(){
+        console.log("111")
+        set_slider(0);
+        return false;
+    });
+
+    $('#in02').click(function(){
+        console.log("222")
+        set_slider(1);
+        return false;
+    });
+
+    $('#in03').click(function(){
+        console.log("333")
+        set_slider(2);
+        return false;
+    });
+    
+    
+
+    
 }); 
+
